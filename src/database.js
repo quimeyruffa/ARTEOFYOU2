c0596e2d5ba20924c386b3d6f25137ac9f47fe36
kimi no na wa <quimeyzoe@hotmail.com>
2021-03-31 09:59:53 -0300

    error 404

--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
 const moongose = require('mongoose');
+const {database} = require('./keys')
 const {config} = require('dotenv');
 config()
 
-moongose.connect(process.env.MONGODB_URI,{
+moongose.connect(process.env.MONGODB_URI || database.URI ,{
     useNewUrlParser:true
 })
 .then(db => console.log('db is connected'))
---
+++ src/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    database:{
+        URI:'mongodb+srv://quimeyruffa:tsa190700qzrv!!@cluster0.7zjbq.mongodb.net/arteofyou?retryWrites=true&w=majority'
+        
+    }
+}
